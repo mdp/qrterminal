5e0dc3ed18293ba5dd3f210e5972dcd619b628fb
mdp <m@mdp.im>
2016-01-31 08:38:45 -0800

    Black on white background

--- qrterminal.go
+++ qrterminal.go
@@ -6,8 +6,8 @@ import (
 	"code.google.com/p/rsc/qr"
 )
 
-const WHITE = "\033[40m  \033[0m"
-const BLACK = "\033[47m  \033[0m"
+const BLACK = "\033[40m  \033[0m"
+const WHITE = "\033[47m  \033[0m"
 
 // Level - the QR Code's redundancy level
 type Level int
@@ -20,18 +20,18 @@ const L = qr.L
 func Generate(text string, l qr.Level, w io.Writer) {
 	code, _ := qr.Encode(text, l)
 	// Frame the barcode in a 1 pixel border
-	w.Write([]byte(BLACK))
+	w.Write([]byte(WHITE))
 	for i := 0; i <= code.Size; i++ {
-		w.Write([]byte(BLACK))
+		w.Write([]byte(WHITE))
 	}
 	w.Write([]byte("\n"))
 	for i := 0; i <= code.Size; i++ {
-		w.Write([]byte(BLACK))
+		w.Write([]byte(WHITE))
 		for j := 0; j <= code.Size; j++ {
 			if code.Black(i, j) {
-				w.Write([]byte(WHITE))
-			} else {
 				w.Write([]byte(BLACK))
+			} else {
+				w.Write([]byte(WHITE))
 			}
 		}
 		w.Write([]byte("\n"))
